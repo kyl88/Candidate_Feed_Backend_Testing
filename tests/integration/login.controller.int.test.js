c02e01de85835b91c53eee3e12d7f38272bae764
Kyle Padayachee <kyle44987@gmail.com>
2023-02-20 12:15:07 +0200

    fixing integration test error

---
+++ tests/integration/login.controller.int.test.js
@@ -0,0 +1,29 @@
+const request = require("supertest");
+const app = require("../../app");
+const newLogin = require("../mock-data/new-login.json");
+
+const endpointUrl = '/login/';
+
+
+describe(endpointUrl, ()=> {
+
+// testing HTTP methods in Rest
+// Post - create new
+// Get - Get existing
+// Put - Update existing
+// Delete - delete existing
+
+   it('POST',async ()=>{
+     const response = await request(app)
+       .post(endpointUrl)
+       .send(newLogin);
+       expect(response.statusCode).toBe(201);
+       expect(response.body.login).toBe(newLogin.login);
+       expect(response.body.done).toBe(newLogin.done);
+       
+
+   });
+
+
+
+});
