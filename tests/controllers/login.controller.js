e6732c6a745bd121f45be903d1ae7b82024d5737
Kyle Padayachee <kyle44987@gmail.com>
2023-02-22 14:50:33 +0200

    fixed errorhandler bug for unit test case

--- tests/controllers/login.controller.js
+++ tests/controllers/login.controller.js
@@ -2,8 +2,13 @@ const LoginModel = require("./model/login.model");
 
 
 exports.createLogin = async (req,res,next) => {
-   const createdModel = await LoginModel.create(req.body);
-   res.status(201).json(createdModel);
+   try {
 
+ 
+       const createdModel = await LoginModel.create(req.body);
+        res.status(201).json(createdModel);
+   } catch (err) {
+        next(err); 
+   }   
 };
 
