3f2ce9456d0f17bf13bc6adad81c80604813a914
Kyle Padayachee <kyle44987@gmail.com>
2023-03-20 14:36:29 +0200

    created a custom Jest master with the setup JS files

---
+++ jest.config.bat
@@ -0,0 +1,6 @@
+module.exports = {
+  transform: {
+    "^.+\\.jsx?$": "babel-jest"
+  },
+  testTimeout: 15000 // <<< this line
+};
--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,8 @@
 module.exports = {
 
-    testEnvironment:'node'
+    testEnvironment:'node',
+    "setupFilesAfterEnv": ["./setupJest.js"]
 
-    
+
 };
 
